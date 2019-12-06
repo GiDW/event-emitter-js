2bdbeb04c46fbce7d9da1f9664d77ef5bc7715f6
Gilles De Waele <gilles.dewaele@basalte.be>
2019-12-06 10:47:45 +0100

    Relax typings for listeners

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,11 @@
 declare class EventEmitter {
   constructor();
-  _events: {[eventName: string]: (() => void)[]};
+  _events: {[eventName: string]: (() => any)[]};
   emit(eventName: string, ...args: any);
-  addListener(eventName: string, listener: (...args: any) => void);
-  removeListener(eventName: string, listener: (...args: any) => void);
+  addListener(eventName: string, listener: (...args: any) => any);
+  removeListener(eventName: string, listener: (...args: any) => any);
   removeAllListeners();
-  on(eventName: string, listener: (...args: any) => void);
-  off(eventName: string, listener: (...args: any) => void);
-  once(eventName: string, listener: (...args: any) => void);
+  on(eventName: string, listener: (...args: any) => any);
+  off(eventName: string, listener: (...args: any) => any);
+  once(eventName: string, listener: (...args: any) => any);
 }

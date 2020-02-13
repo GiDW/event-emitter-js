3f11c4e7206be0f24925e74febc54ccc5c728a2a
Gilles De Waele <gilles.dewaele@basalte.be>
2020-02-13 13:16:47 +0100

    Cleanup typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,11 @@
 declare class EventEmitter {
-  constructor();
-  _events: {[eventName: string]: (() => any)[]};
-  emit(eventName: string, ...args: any);
-  addListener(eventName: string, listener: (...args: any) => any);
-  removeListener(eventName: string, listener: (...args: any) => any);
-  removeAllListeners();
-  on(eventName: string, listener: (...args: any) => any);
-  off(eventName: string, listener: (...args: any) => any);
-  once(eventName: string, listener: (...args: any) => any);
+  constructor()
+  _events: {[eventName: string]: (() => any)[]}
+  emit(eventName: string, ...args: any)
+  addListener(eventName: string, listener: (...args: any) => any)
+  removeListener(eventName: string, listener: (...args: any) => any)
+  removeAllListeners()
+  on(eventName: string, listener: (...args: any) => any)
+  off(eventName: string, listener: (...args: any) => any)
+  once(eventName: string, listener: (...args: any) => any)
 }

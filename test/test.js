903ec7252d8c99b988eb508c1217c9c8d81146ee
Gilles De Waele <gilles.dewaele@basalte.be>
2020-02-13 13:15:32 +0100

    Test both minified and regular build

--- test/test.js
+++ test/test.js
@@ -4,120 +4,123 @@
 
 var assert = require('assert')
 
-var EventEmitter = require('../dist/event_emitter').default
+testEventEmitter(require('../dist/event_emitter').default, 'EventEmitter')
+testEventEmitter(require('../dist/index.min'), 'EventEmitter (minified)')
 
-describe('EventEmitter', function () {
-  describe('#on', function () {
-    it('should fire once', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let count = 0
-      emitter.on(EVT, function () {
-        count++
+function testEventEmitter (EventEmitter, testName) {
+  describe(testName, function () {
+    describe('#on', function () {
+      it('should fire once', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let count = 0
+        emitter.on(EVT, function () {
+          count++
+        })
+        emitter.emit(EVT)
+        assert.strictEqual(count, 1)
       })
-      emitter.emit(EVT)
-      assert.strictEqual(count, 1)
-    })
-    it('should fire twice', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let count = 0
-      emitter.on(EVT, function () {
-        count++
+      it('should fire twice', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let count = 0
+        emitter.on(EVT, function () {
+          count++
+        })
+        emitter.on(EVT, function () {
+          count++
+        })
+        emitter.emit(EVT)
+        assert.strictEqual(count, 2)
       })
-      emitter.on(EVT, function () {
-        count++
+      it('should fire four times', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let count = 0
+        emitter.on(EVT, function () {
+          count++
+        })
+        emitter.on(EVT, function () {
+          count++
+        })
+        emitter.emit(EVT)
+        emitter.emit(EVT)
+        assert.strictEqual(count, 4)
       })
-      emitter.emit(EVT)
-      assert.strictEqual(count, 2)
-    })
-    it('should fire four times', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let count = 0
-      emitter.on(EVT, function () {
-        count++
-      })
-      emitter.on(EVT, function () {
-        count++
-      })
-      emitter.emit(EVT)
-      emitter.emit(EVT)
-      assert.strictEqual(count, 4)
-    })
-    it('should fire once', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let count = 0
-      emitter.on(EVT, listener)
-      emitter.emit(EVT)
-      emitter.removeListener(EVT, listener)
-      emitter.emit(EVT)
-      assert.strictEqual(count, 1)
+      it('should fire once', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let count = 0
+        emitter.on(EVT, listener)
+        emitter.emit(EVT)
+        emitter.removeListener(EVT, listener)
+        emitter.emit(EVT)
+        assert.strictEqual(count, 1)
 
-      function listener () {
-        count++
-      }
-    })
-    it('should pass event arguments', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let result1, result2
-      emitter.on(EVT, function (arg1, arg2) {
-        result1 = arg1
-        result2 = arg2
+        function listener () {
+          count++
+        }
       })
-      emitter.emit(EVT, 12, 'lol')
-      assert.strictEqual(result1, 12)
-      assert.strictEqual(result2, 'lol')
-    })
-    it('should have the emitter "this" context', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let listenerThis
-      emitter.on(EVT, function () {
-        listenerThis = this
+      it('should pass event arguments', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let result1, result2
+        emitter.on(EVT, function (arg1, arg2) {
+          result1 = arg1
+          result2 = arg2
+        })
+        emitter.emit(EVT, 12, 'lol')
+        assert.strictEqual(result1, 12)
+        assert.strictEqual(result2, 'lol')
       })
-      emitter.emit(EVT)
-      assert.strictEqual(listenerThis, emitter)
+      it('should have the emitter "this" context', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let listenerThis
+        emitter.on(EVT, function () {
+          listenerThis = this
+        })
+        emitter.emit(EVT)
+        assert.strictEqual(listenerThis, emitter)
+      })
+    })
+
+    describe('#once', function () {
+      it('should fire once', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let count = 0
+        emitter.once(EVT, function () {
+          count++
+        })
+        emitter.emit(EVT)
+        emitter.emit(EVT)
+        assert.strictEqual(count, 1)
+      })
+      it('should fire once for each "once" listener', function () {
+        const EVT = 'someEvent'
+        const emitter = new EventEmitter()
+        let count1 = 0
+        let count2 = 0
+        let count3 = 0
+        emitter.emit(EVT)
+        emitter.once(EVT, function () {
+          count1++
+        })
+        emitter.emit(EVT)
+        emitter.on(EVT, function () {
+          count2++
+        })
+        emitter.emit(EVT)
+        emitter.once(EVT, function () {
+          count3++
+        })
+        emitter.emit(EVT)
+        emitter.emit(EVT)
+        assert.strictEqual(count1, 1)
+        assert.strictEqual(count2, 3)
+        assert.strictEqual(count3, 1)
+      })
     })
   })
-
-  describe('#once', function () {
-    it('should fire once', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let count = 0
-      emitter.once(EVT, function () {
-        count++
-      })
-      emitter.emit(EVT)
-      emitter.emit(EVT)
-      assert.strictEqual(count, 1)
-    })
-    it('should fire once for each "once" listener', function () {
-      const EVT = 'someEvent'
-      const emitter = new EventEmitter()
-      let count1 = 0
-      let count2 = 0
-      let count3 = 0
-      emitter.emit(EVT)
-      emitter.once(EVT, function () {
-        count1++
-      })
-      emitter.emit(EVT)
-      emitter.on(EVT, function () {
-        count2++
-      })
-      emitter.emit(EVT)
-      emitter.once(EVT, function () {
-        count3++
-      })
-      emitter.emit(EVT)
-      emitter.emit(EVT)
-      assert.strictEqual(count1, 1)
-      assert.strictEqual(count2, 3)
-      assert.strictEqual(count3, 1)
-    })
-  })
-})
+}

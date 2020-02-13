48b2d665e9b6c8ff64b8639d245cb3c9d81db16d
Gilles De Waele <gilles.dewaele@basalte.be>
2020-02-13 13:38:59 +0100

    Made typings more compliant

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,12 @@
+export = EventEmitter
 declare class EventEmitter {
   constructor()
-  _events: {[eventName: string]: (() => any)[]}
-  emit(eventName: string, ...args: any)
-  addListener(eventName: string, listener: (...args: any) => any)
-  removeListener(eventName: string, listener: (...args: any) => any)
-  removeAllListeners()
-  on(eventName: string, listener: (...args: any) => any)
-  off(eventName: string, listener: (...args: any) => any)
-  once(eventName: string, listener: (...args: any) => any)
+  _events: {[eventName: string]: ((...args: any) => void)[]}
+  emit(eventName: string, ...args: any): this
+  addListener(eventName: string, listener: (...args: any) => void): this
+  removeListener(eventName: string, listener: (...args: any) => void): this
+  removeAllListeners(): this
+  on(eventName: string, listener: (...args: any) => void): this
+  off(eventName: string, listener: (...args: any) => void): this
+  once(eventName: string, listener: (...args: any) => void): this
 }

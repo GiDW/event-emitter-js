753f770e3e8fa96e07f2fc5febd3f495d2c5bd64
Gilles De Waele <de.waele.gilles@gmail.com>
2021-02-08 10:36:03 +0100

    All rest parameters are arrays

--- types/event_emitter.d.ts
+++ types/event_emitter.d.ts
@@ -1,12 +1,12 @@
 declare class EventEmitter {
-  private _events: { [key: string]: ((...args: any) => void)[] }
+  private _events: { [key: string]: ((...args: any[]) => void)[] }
   emit(eventName: string, ...args: any[]): this
-  addListener(eventName: string, listener: (...args: any) => void): this
-  removeListener(eventName: string, listener: (...args: any) => void): this
-  on(eventName: string, listener: (...args: any) => void): this
-  off(eventName: string, listener: (...args: any) => void): this
+  addListener(eventName: string, listener: (...args: any[]) => void): this
+  removeListener(eventName: string, listener: (...args: any[]) => void): this
+  on(eventName: string, listener: (...args: any[]) => void): this
+  off(eventName: string, listener: (...args: any[]) => void): this
   removeAllListeners(): this
-  once(eventName: string, listener: (...args: any) => void): this
+  once(eventName: string, listener: (...args: any[]) => void): this
   private _once
 }
 export default EventEmitter
